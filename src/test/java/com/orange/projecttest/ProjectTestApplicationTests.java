02d3969e149e3d3aba48263991d7d259d1501796
gabrielmihai98 <gabriel.isleam@gmail.com>
2019-11-15 09:59:27 +0200

    Initial commit

---
+++ src/test/java/com/orange/projecttest/ProjectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.projecttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
